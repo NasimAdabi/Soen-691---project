495af778a787e0c64bc3114a769351dad91dd78e
samanehshirdel <50811721+samanehshirdel@users.noreply.github.com>
2020-04-27 16:36:44 -0400

    Add files via upload

---
+++ changeMetric/changeMetric.js
@@ -0,0 +1,19 @@
+var fs = require('fs');
+
+require.extensions['.txt'] = function (module, filename) {
+    module.exports = fs.readFileSync(filename, 'utf8');
+};
+
+var changes = require("./change1.txt").toString();
+
+let result = changes.split('\n').map((q) => {
+    return /([^\/]+)(\.java)/.test(q) ? `${/([^\/]+)(\.java)/.exec(q)[0]},${/[0-9]+/.test(q) ? /[0-9]+/.exec(q)[0] : ''}` : null;
+}).filter(q => q !== null).join('\n');
+
+fs.writeFile("changeMetrics.csv", result, function (err) {
+    if (err) {
+        return console.log(err);
+    }
+    console.log("The file was saved!");
+});
+
